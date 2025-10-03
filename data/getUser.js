612fc2a4a3f9a70e3c9626612c49b0ba875eddb5
avdsp <avdhoot.patil@codeblaze.ae>
2025-10-03 12:26:49 +0530

    Setup and created user crud endpoint with hardcoded data

---
+++ data/getUser.js
@@ -0,0 +1,18 @@
+module.exports = () => {
+  return {
+    id: "1",
+    name: "Ethan Harper",
+    firstName: "Ethan",
+    lastName: "Harper",
+    email: "ethan.harper@example.com",
+    role: "admin",
+    createdDate: "2023-01-15", // keep as string unless converted to Date
+    status: "Active",
+    phoneNumber: "8444444444",
+    jobTitle: "Software Engineer",
+    languagePreference: "English",
+    address: "123 Main St",
+    city: "New York",
+    postcode: "10001",
+  };
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+//create user
+app.post("/api/v1/user", function (req, res) {
+  console.log("/v1/createUser");
+  res.status(201).send({ message: "User created successfully" });
+});
+
+//update user
+app.put("/api/v1/user/:id", function (req, res) {
+  console.log("/v1/updateUser");
+  res.status(200).send({ message: "User updated successfully" });
+});
+
+//delete user
+app.delete("/api/v1/user/:id", function (req, res) {
+  console.log("/v1/deleteUser");
+  res.status(200).send({ message: "User deleted successfully" });
+});
+
+//get user
+app.get("/api/v1/user/:id", function (req, res) {
+  console.log("/v1/getUser");
+  const getUser = require("./data/getUser");
+  res.status(200).send(getUser());
+});
+
+//list users
+app.get("/api/v1/users", function (req, res) {
+  console.log("/v1/getUserList");
+  const getUserList = require("./data/getUserList");
+  res.status(200).send(getUserList());
+});
+
+// Start the server
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
